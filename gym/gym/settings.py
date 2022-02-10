d9503cbf051c7b6a2bd50e67cf590bc4a2807af5
Ernest <muiboonyang@gmail.com>
2022-02-11 01:53:00 +0800

    Edits

--- gym/gym/settings.py
+++ gym/gym/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://anywhere-fitness-first.herokuapp.com/']
+ALLOWED_HOSTS = ['anywhere-fitness-first.herokuapp.com', '127.0.0.1']
 
 AUTH_USER_MODEL = 'accounts.Account'
 AUTHENTICATION_BACKENDS = (
