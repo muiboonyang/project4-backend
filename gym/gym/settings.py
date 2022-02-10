4307a581ed6ce00b7f046d8ce812d9e4195dce2b
Ernest <muiboonyang@gmail.com>
2022-02-10 23:02:16 +0800

    Updated backend

--- gym/gym/settings.py
+++ gym/gym/settings.py
@@ -26,7 +26,8 @@ load_dotenv(find_dotenv())
 
 # UPDATE secret key
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
