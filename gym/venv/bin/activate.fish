978cb76b3d51703a6b28c7b47a3579f78d3c543e
Ernest <muiboonyang@gmail.com>
2022-03-02 11:04:35 +0800

    Update backend

--- gym/venv/bin/activate.fish
+++ gym/venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ernest/Documents/GitHub/project4-backend/gym/venv"
+set -gx VIRTUAL_ENV "/Users/ernest/Documents/GitHub/my-projects/project4-backend/gym/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
